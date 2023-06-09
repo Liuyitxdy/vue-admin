b471bd78f36008f48d2f2b25ea1ede70b3a2c620
zz77800 <2940742049@qq.com>
2023-06-09 19:12:02 +0800

    mange文件完成

--- src/views/mange/Admin.vue
+++ src/views/mange/Admin.vue
@@ -1,9 +1,52 @@
 <template>
-  <div>管理员</div>
+  <div>
+    <!-- 管理员 -->
+    <el-table
+      :data="anminlist"
+      style="width: 100%">
+      <el-table-column
+        prop="administratorName"
+        label="姓名"
+        width="180" align="center">
+      </el-table-column>
+      <el-table-column
+        prop="dateOfRegistration"
+        label="日期"
+        width="180" align="center">
+      </el-table-column>
+      <el-table-column
+        prop="administratorAddress"
+        label="地址" align="center">
+      </el-table-column>
+      <el-table-column
+        prop="might"
+        label="管理员" align="center"
+        >
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
-export default {};
+import http from "@/api/http.js";
+
+export default {
+ data(){
+  return{
+    anminlist:[]
+  }
+ },
+ mounted(){
+  http.get("/data.json").then((res) => {
+      console.log(res.data.data.adminiList);
+      this.anminlist = res.data.data.adminiList
+    });
+  },
+ }
 </script>
 
-<style></style>
+<style lang="less" scoped>
+
+
+
+</style>
--- src/views/mange/Orders.vue
+++ src/views/mange/Orders.vue
@@ -1,9 +1,76 @@
 <template>
-  <div>订单</div>
+
+  <div>
+    <!-- 订单 -->
+    <el-table
+    :data="ordersList"
+    style="width:100%">
+    <el-table-column type="expand">
+      <template slot-scope="props">
+        <el-form label-position="left" inline class="demo-table-expand">
+          <el-form-item label="用户名">
+            <span>{{ props.row.name}}</span>
+          </el-form-item>
+          <el-form-item label="店铺名称">
+            <span>{{ props.row.shopname }}</span>
+          </el-form-item>
+          <el-form-item label="收货地址">
+            <span>{{ props.row.shopdz }}</span>
+          </el-form-item>
+          <el-form-item label="店铺 ID">
+            <span>{{ props.row.shopid }}</span>
+          </el-form-item>
+          <el-form-item label="店铺地址">
+            <span>{{ props.row.receiptdz }}</span>
+          </el-form-item>
+        </el-form>
+      </template>
+    </el-table-column>
+    <el-table-column
+      label="订单 ID"
+      prop="ordersid" align="center">
+    </el-table-column>
+    <el-table-column
+      label="总价格"
+      prop="pricrsum" align="center">
+    </el-table-column>
+    <el-table-column
+      label="订单状态"
+      prop="orderstatus" align="center">
+    </el-table-column>
+  </el-table>
+  </div>
 </template>
 
 <script>
-export default {};
+import http from "@/api/http.js";
+
+export default {
+ data(){
+  return{
+    ordersList:[]
+  }
+ },
+ mounted(){
+  http.get("/data.json").then((res) => {
+      console.log(res.data.data.ordersList);
+      this.ordersList = res.data.data.ordersList
+    });
+  },
+ }
 </script>
 
-<style></style>
+<style lang="css" scoped>
+.demo-table-expand {
+    font-size: 0;
+  }
+  .demo-table-expand label {
+    width: 90px;
+    color: #99a9bf;
+  }
+  .demo-table-expand .el-form-item {
+    margin-right: 0;
+    margin-bottom: 0;
+    width: 50%;
+  }
+</style>
