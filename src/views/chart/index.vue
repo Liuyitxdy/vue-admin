1477f48eceb2697988407623b26f786fd4919ba4
wangyan <1551718240@qq.com>
2023-06-11 11:26:37 +0800

    编辑和图标

--- src/views/chart/index.vue
+++ src/views/chart/index.vue
@@ -1,9 +1,110 @@
 <template>
-  <div>图表</div>
+  <div ref="chart" style="width: 1200px; height: 400px" class="chart"></div>
 </template>
 
 <script>
-export default {};
+import * as echarts from "echarts";
+import http from "@/api/http";
+import { toRaw } from "vue";
+
+export default {
+  mounted() {
+    // 在组件挂载后，初始化 ECharts 实例，并设置配置项
+    this.chart = echarts.init(this.$refs.chart);
+    setTimeout(() => {
+      this.updateChart();
+    }, 1000);
+    this.getData();
+  },
+  data() {
+    return {
+      dates: [],
+      chartData: [],
+      addUsers: [],
+      addOrders: [],
+      addAdmin: [],
+    };
+  },
+  methods: {
+    getData() {
+      http
+        .get("/data.json")
+        .then((res) => {
+          this.chartData = res.data.data.dayData;
+          this.addUsers = [];
+          this.chartData.forEach((item) => {
+            this.addUsers.push(item.addUser);
+            this.addOrders.push(item.addOrders);
+            this.addAdmin.push(item.addAdmin);
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+
+      console.log(this.addUsers);
+    },
+    updateChart() {
+      // 更新图表配置
+      for (let i = 6; i >= 0; i--) {
+        const date = new Date();
+        date.setDate(date.getDate() - i);
+        const year = date.getFullYear();
+        const month = date.getMonth() + 1;
+        const day = date.getDate();
+        this.dates.push(`${year}-${month}-${day}`);
+      }
+      console.log(this.dates);
+      const option = {
+
+            title: {
+              text: '用户分布',
+              subtext: ' ',
+              left: 'center'
+            },
+            tooltip: {
+              trigger: 'item'
+            },
+            legend: {
+              orient: 'vertical',
+              left: 'left'
+            },
+            series: [
+              {
+                name: 'Access From',
+                type: 'pie',
+                radius: '50%',
+                data: [
+                  { value: 1048, name: '北京' },
+                  { value: 735, name: '上海' },
+                  { value: 580, name: '深圳' },
+                  { value: 484, name: '成都' },
+                  { value: 300, name: '其他' }
+                ],
+                emphasis: {
+                  itemStyle: {
+                    shadowBlur: 10,
+                    shadowOffsetX: 0,
+                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+                  }
+                }
+              }
+            ]
+          };
+
+          this.chart.setOption(option);
+    },
+  },
+  beforeDestroy() {
+    // 在组件销毁前销毁 ECharts 实例
+    this.chart.dispose();
+  },
+};
 </script>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.chart {
+  margin: 0 auto;
+  margin-top: 50px;
+}
+</style>
--- src/views/edit/index.vue
+++ src/views/edit/index.vue
@@ -1,9 +1,82 @@
 <template>
-  <div>编辑</div>
+  <div class="home">
+    <mavon-editor :codeStyle="codeStyle" :toolbars="toolbars" v-model="content" :ishljs="true" ref="md" @change="change"/>
+    <el-button @click="outputENter" class="btn">提交</el-button>
+  </div>
 </template>
 
 <script>
-export default {};
-</script>
+export default {
+  data() {
+    return {
+      content: " ",
+      page_article:undefined,
+      html_content:undefined,
+      md_content:undefined,
+      toolbars: {
+        bold: true, // 粗体
+        italic: true, // 斜体
+        header: true, // 标题
+        underline: true, // 下划线
+        strikethrough: true, // 中划线
+        mark: true, // 标记
+        superscript: true, // 上角标
+        subscript: true, // 下角标
+        quote: true, // 引用
+        ol: true, // 有序列表
+        ul: true, // 无序列表
+        link: true, // 链接
+        imagelink: true, // 图片链接
+        code: true, // code
+        table: true, // 表格
+        fullscreen: true, // 全屏编辑
+        readmodel: true, // 沉浸式阅读
+        htmlcode: true, // 展示html源码
+        help: true, // 帮助
+        undo: true, // 上一步
+        redo: true, // 下一步
+        trash: true, // 清空
+        save: true, // 保存（触发events中的save事件）
+        navigation: true, // 导航目录
+        alignleft: true, // 左对齐
+        aligncenter: true, // 居中
+        alignright: true, // 右对齐
+        subfield: true, // 单双栏模式
+        preview: true // 预览
+      },
+      codeStyle:'monokai-sublime',//主题
+      test_html:undefined
+    };
+  },
+  methods:{
+    outputENter(){
+      // console.log(JSON.stringify(this.content))
+      // console.log(this.$refs.md.d_render)
+      // console.log(this.$refs.md.d_value)
+      console.log(JSON.stringify(this.test_html))   //就用这个存数据库  然后进行回显即可
+      this.$axios.post("http://127.0.0.1:3009",{
+        params:{
+          msg:this.test_html
+        }
+      }).then((res)=>{
+        console.log(res)
+        console.log("响应成功")
+      })
 
-<style></style>
+    },
+    change(value, render){
+      // render 为 markdown 解析后的结果
+      this.test_html = render;
+    }
+  }
+};
+</script>
+<style>
+.btn{
+  margin-top: 80px;
+  margin-left: 600px;
+  background-color: #20a0ff;
+  color: #ffffff;
+}
+</style>
+
