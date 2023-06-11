432eb972c102eec942e37a91dc8d4c4ccefdc359
Liuyitxdy <95669425+Liuyitxdy@users.noreply.github.com>
2023-06-11 20:06:34 +0800

    修改登录页逻辑

--- src/views/addData/addShop.vue
+++ src/views/addData/addShop.vue
@@ -1,66 +1,61 @@
 <template>
   <div class="app-container">
-    <div style="border: 1px solid #ccc;margin-bottom: 50px;">
-      上面盒子
-    </div>
-
-     <div style="border: 1px solid #ccc;">
+    <div style="border: 1px solid #ccc">
       <h3 class="tianjiasp">添加商品</h3>
-    <el-form
-      :model="ruleForm"
-      :rules="rules"
-      ref="ruleForm"
-      label-width="100px"
-      class="demo-ruleForm"
-    >
-      <el-form-item label="商品名称" prop="name" :rules="nameRules" required>
-        <el-input v-model="ruleForm.name"></el-input>
-      </el-form-item>
-      <el-form-item label="食品活动" prop="info">
-        <el-input v-model="ruleForm.info"></el-input>
-      </el-form-item>
-      <el-form-item label="食品详情" prop="info">
-        <el-input v-model="ruleForm.info"></el-input>
-      </el-form-item>
-      <el-dialog :visible.sync="dialogVisible">
-        <img width="100%" :src="dialogImageUrl" alt="">
-      </el-dialog>
-      <el-form-item label="食品特点" prop="region">
-        <el-select v-model="ruleForm.region" placeholder="请选择">
-          <el-option label="特点一" value="shanghai"></el-option>
-          <el-option label="特点二" value="beijing"></el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="食品规格" prop="radio">
-        <el-radio-group v-model="ruleForm.radio">
-          <el-radio :label="1">单规格</el-radio>
-          <el-radio :label="2">多规格</el-radio>
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item label="包装费" prop="psf">
-        <el-input-number
-          v-model="ruleForm.psf"
-          @change="handleChange"
-          :min="1"
-          label="包装费"
-        ></el-input-number>
-      </el-form-item>
-      <el-form-item label="价格" prop="qsj">
-        <el-input-number
-          v-model="ruleForm.qsj"
-          @change="handleChange"
-          :min="1"
-          label="价格"
-        ></el-input-number>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="submitForm('ruleForm')">
-          确定添加商品
-        </el-button>
-      </el-form-item>
-    </el-form>
-     </div>
-    
+      <el-form
+        :model="ruleForm"
+        :rules="rules"
+        ref="ruleForm"
+        label-width="100px"
+        class="demo-ruleForm"
+      >
+        <el-form-item label="商品名称" prop="name" :rules="nameRules" required>
+          <el-input v-model="ruleForm.name"></el-input>
+        </el-form-item>
+        <el-form-item label="食品活动" prop="info">
+          <el-input v-model="ruleForm.info"></el-input>
+        </el-form-item>
+        <el-form-item label="食品详情" prop="info">
+          <el-input v-model="ruleForm.info"></el-input>
+        </el-form-item>
+        <el-dialog :visible.sync="dialogVisible">
+          <img width="100%" :src="dialogImageUrl" alt="" />
+        </el-dialog>
+        <el-form-item label="食品特点" prop="region">
+          <el-select v-model="ruleForm.region" placeholder="请选择">
+            <el-option label="特点一" value="shanghai"></el-option>
+            <el-option label="特点二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="食品规格" prop="radio">
+          <el-radio-group v-model="ruleForm.radio">
+            <el-radio :label="1">单规格</el-radio>
+            <el-radio :label="2">多规格</el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="包装费" prop="psf">
+          <el-input-number
+            v-model="ruleForm.psf"
+            @change="handleChange"
+            :min="1"
+            label="包装费"
+          ></el-input-number>
+        </el-form-item>
+        <el-form-item label="价格" prop="qsj">
+          <el-input-number
+            v-model="ruleForm.qsj"
+            @change="handleChange"
+            :min="1"
+            label="价格"
+          ></el-input-number>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('ruleForm')">
+            确定添加商品
+          </el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
@@ -81,7 +76,6 @@ export default {
       nameRules: [
         { required: true, message: "请输入商品名称", trigger: "blur" },
       ],
-     
     };
   },
   methods: {
@@ -109,7 +103,7 @@ export default {
 <style lang="scss" scoped>
 .app-container {
   width: 800px;
-  margin: 0 auto; 
+  margin: 0 auto;
   padding: 20px;
 }
 
@@ -125,7 +119,6 @@ export default {
   content: "*";
   color: red;
 }
-
 
 .top-line {
   height: 3px;
@@ -177,6 +170,4 @@ export default {
 .app-container {
   /* 原有的样式 */
 }
-
-
 </style>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="login-container">
-    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
-
+    <el-form
+      ref="loginForm"
+      :model="loginForm"
+      :rules="loginRules"
+      class="login-form"
+      auto-complete="on"
+      label-position="left"
+    >
       <div class="title-container">
         <h3 class="title">elm后台管理系统</h3>
       </div>
@@ -37,110 +43,134 @@
           @keyup.enter.native="handleLogin"
         />
         <span class="show-pwd" @click="showPwd">
-          <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
+          <svg-icon
+            :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'"
+          />
         </span>
       </el-form-item>
 
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin" @click="open2">登录</el-button>
+      <el-button
+        :loading="loading"
+        type="primary"
+        style="width: 100%; margin-bottom: 30px"
+        @click.native.prevent="handleLogin"
+        @click="open2"
+        >登录</el-button
+      >
 
       <div class="tips">
-        <span style="margin-right:20px;text-align: center;display: block;color: red">温馨提示：</span>
-        <p style="text-align: center;color: red">未登录过的新用户，自动注册<br>主册过的用户可凭账号密码登亏</p>
+        <span
+          style="
+            margin-right: 20px;
+            text-align: center;
+            display: block;
+            color: red;
+          "
+          >温馨提示：</span
+        >
+        <p style="text-align: center; color: red">
+          未登录过的新用户，自动注册<br />主册过的用户可凭账号密码登亏
+        </p>
       </div>
-
     </el-form>
   </div>
 </template>
 
 <script>
-import { validUsername } from '@/utils/validate'
+import { validUsername } from "@/utils/validate";
+import http from "@/api/http";
 
 export default {
-  name: 'Login',
+  name: "Login",
   data() {
     const validateUsername = (rule, value, callback) => {
       if (!validUsername(value)) {
-        callback(new Error('Please enter the correct user name'))
+        callback(new Error("请输入用户名"));
       } else {
-        callback()
+        callback();
       }
-    }
+    };
     const validatePassword = (rule, value, callback) => {
       if (value.length < 6) {
-        callback(new Error('The password can not be less than 6 digits'))
+        callback(new Error("密码不少于6位数"));
       } else {
-        callback()
+        callback();
       }
-    }
+    };
     return {
       loginForm: {
-        username: 'admin',
-        password: '111111'
+        username: "",
+        password: "",
       },
       loginRules: {
-        username: [{ required: true, trigger: 'blur', validator: validateUsername }],
-        password: [{ required: true, trigger: 'blur', validator: validatePassword }]
+        username: [
+          { required: true, trigger: "blur", validator: validateUsername },
+        ],
+        password: [
+          { required: true, trigger: "blur", validator: validatePassword },
+        ],
       },
       loading: false,
-      passwordType: 'password',
-      redirect: undefined
-    }
+      passwordType: "password",
+      redirect: undefined,
+    };
   },
   watch: {
     $route: {
-      handler: function(route) {
-        this.redirect = route.query && route.query.redirect
+      handler: function (route) {
+        this.redirect = route.query && route.query.redirect;
       },
-      immediate: true
-    }
+      immediate: true,
+    },
   },
   methods: {
     showPwd() {
-      if (this.passwordType === 'password') {
-        this.passwordType = ''
+      if (this.passwordType === "password") {
+        this.passwordType = "";
       } else {
-        this.passwordType = 'password'
+        this.passwordType = "password";
       }
       this.$nextTick(() => {
-        this.$refs.password.focus()
-      })
+        this.$refs.password.focus();
+      });
     },
     handleLogin() {
-      setTimeout(()=>{
-this.$refs.loginForm.validate(valid => {
-        if (valid) {
-          this.loading = true
-          this.$store.dispatch('user/login', this.loginForm).then(() => {
-            this.$router.push({ path: this.redirect || '/' })
-            this.loading = false
-          }).catch(() => {
-            this.loading = false
-          })
-        } else {
-          console.log('error submit!!')
-          return false
-        }
-      })
-      },2000)
-
+      setTimeout(() => {
+        this.$refs.loginForm.validate((valid) => {
+          if (valid) {
+            this.loading = true;
+            this.$store
+              .dispatch("user/login", this.loginForm)
+              .then(() => {
+                this.$router.push({ path: this.redirect || "/" });
+                this.loading = false;
+              })
+              .catch(() => {
+                this.loading = false;
+              });
+          } else {
+            console.log("error submit!!");
+            return false;
+          }
+        });
+      }, 2000);
     },
     open2() {
-        this.$message({
-          message: '登录成功',
-          type: 'success',
-
-        });
-      },
-  }
-}
+      http.get("/data.json").then((res) => {
+        console.log(res.data.data.admin);
+      });
+      this.$message({
+        message: "登录成功",
+        type: "success",
+      });
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-/* 修复input 背景不协调 和光标变色 */
-/* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
-
-$bg:#283443;
-$light_gray:#fff;
+$bg: #283443;
+$light_gray: #fff;
 $cursor: #fff;
 
 @supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
@@ -183,9 +213,9 @@ $cursor: #fff;
 </style>
 
 <style lang="scss" scoped>
-$bg:#2d3a4b;
-$dark_gray:#889aa4;
-$light_gray:#eee;
+$bg: #2d3a4b;
+$dark_gray: #889aa4;
+$light_gray: #eee;
 
 .login-container {
   min-height: 100%;
