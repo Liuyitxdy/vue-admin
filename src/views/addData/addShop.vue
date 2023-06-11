4c8a0b8585be6f86ec2f54c760a3b40dd7b81513
mengziw <2385507691@qq.com>
2023-06-11 19:23:54 +0800

    添加商品

--- src/views/addData/addShop.vue
+++ src/views/addData/addShop.vue
@@ -1,59 +1,66 @@
 <template>
   <div class="app-container">
-    <el-form ref="form" :model="form" label-width="120px">
-      <el-form-item label="Activity name">
-        <el-input v-model="form.name" />
+    <div style="border: 1px solid #ccc;margin-bottom: 50px;">
+      上面盒子
+    </div>
+
+     <div style="border: 1px solid #ccc;">
+      <h3 class="tianjiasp">添加商品</h3>
+    <el-form
+      :model="ruleForm"
+      :rules="rules"
+      ref="ruleForm"
+      label-width="100px"
+      class="demo-ruleForm"
+    >
+      <el-form-item label="商品名称" prop="name" :rules="nameRules" required>
+        <el-input v-model="ruleForm.name"></el-input>
       </el-form-item>
-      <el-form-item label="Activity zone">
-        <el-select v-model="form.region" placeholder="please select your zone">
-          <el-option label="Zone one" value="shanghai" />
-          <el-option label="Zone two" value="beijing" />
+      <el-form-item label="食品活动" prop="info">
+        <el-input v-model="ruleForm.info"></el-input>
+      </el-form-item>
+      <el-form-item label="食品详情" prop="info">
+        <el-input v-model="ruleForm.info"></el-input>
+      </el-form-item>
+      <el-dialog :visible.sync="dialogVisible">
+        <img width="100%" :src="dialogImageUrl" alt="">
+      </el-dialog>
+      <el-form-item label="食品特点" prop="region">
+        <el-select v-model="ruleForm.region" placeholder="请选择">
+          <el-option label="特点一" value="shanghai"></el-option>
+          <el-option label="特点二" value="beijing"></el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="Activity time">
-        <el-col :span="11">
-          <el-date-picker
-            v-model="form.date1"
-            type="date"
-            placeholder="Pick a date"
-            style="width: 100%"
-          />
-        </el-col>
-        <el-col :span="2" class="line">-</el-col>
-        <el-col :span="11">
-          <el-time-picker
-            v-model="form.date2"
-            type="fixed-time"
-            placeholder="Pick a time"
-            style="width: 100%"
-          />
-        </el-col>
-      </el-form-item>
-      <el-form-item label="Instant delivery">
-        <el-switch v-model="form.delivery" />
-      </el-form-item>
-      <el-form-item label="Activity type">
-        <el-checkbox-group v-model="form.type">
-          <el-checkbox label="Online activities" name="type" />
-          <el-checkbox label="Promotion activities" name="type" />
-          <el-checkbox label="Offline activities" name="type" />
-          <el-checkbox label="Simple brand exposure" name="type" />
-        </el-checkbox-group>
-      </el-form-item>
-      <el-form-item label="Resources">
-        <el-radio-group v-model="form.resource">
-          <el-radio label="Sponsor" />
-          <el-radio label="Venue" />
+      <el-form-item label="食品规格" prop="radio">
+        <el-radio-group v-model="ruleForm.radio">
+          <el-radio :label="1">单规格</el-radio>
+          <el-radio :label="2">多规格</el-radio>
         </el-radio-group>
       </el-form-item>
-      <el-form-item label="Activity form">
-        <el-input v-model="form.desc" type="textarea" />
+      <el-form-item label="包装费" prop="psf">
+        <el-input-number
+          v-model="ruleForm.psf"
+          @change="handleChange"
+          :min="1"
+          label="包装费"
+        ></el-input-number>
+      </el-form-item>
+      <el-form-item label="价格" prop="qsj">
+        <el-input-number
+          v-model="ruleForm.qsj"
+          @change="handleChange"
+          :min="1"
+          label="价格"
+        ></el-input-number>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="onSubmit">Create</el-button>
-        <el-button @click="onCancel">Cancel</el-button>
+        <el-button type="primary" @click="submitForm('ruleForm')">
+          确定添加商品
+        </el-button>
       </el-form-item>
     </el-form>
+     </div>
+    
   </div>
 </template>
 
@@ -61,34 +68,115 @@
 export default {
   data() {
     return {
-      form: {
+      ruleForm: {
         name: "",
+        info: "",
         region: "",
-        date1: "",
-        date2: "",
-        delivery: false,
-        type: [],
-        resource: "",
-        desc: "",
+        radio: "",
+        psf: 1,
+        qsj: 20,
       },
+      dialogVisible: false,
+      dialogImageUrl: "",
+      nameRules: [
+        { required: true, message: "请输入商品名称", trigger: "blur" },
+      ],
+     
     };
   },
   methods: {
-    onSubmit() {
-      this.$message("submit!");
+    handleChange(value) {
+      console.log(value);
     },
-    onCancel() {
-      this.$message({
-        message: "cancel!",
-        type: "warning",
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.$message({
+            message: "创建成功",
+            type: "success",
+          });
+          this.$router.go(0);
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
       });
     },
   },
 };
 </script>
 
-<style scoped>
-.line {
-  text-align: center;
+<style lang="scss" scoped>
+.app-container {
+  width: 800px;
+  margin: 0 auto; 
+  padding: 20px;
 }
+
+.tianjiasp {
+  text-align: center;
+  color: rgb(45, 45, 45);
+  margin-bottom: 20px;
+  border-bottom: none;
+  padding-bottom: 10px;
+}
+
+.el-form-item.required .el-form-item__label:before {
+  content: "*";
+  color: red;
+}
+
+
+.top-line {
+  height: 3px;
+  width: 100%;
+  background-color: #000;
+}
+
+.outer-container {
+  width: 800px;
+  margin: 0 auto;
+  border: 1px solid #b7b4b4;
+  padding: 20px;
+}
+
+.category-container {
+  display: flex;
+  align-items: center;
+}
+
+.category-left {
+  flex: 1;
+}
+
+.category-right {
+  flex: 1;
+  text-align: right;
+}
+
+.add-category-container {
+  display: flex;
+  align-items: center;
+  margin-top: 10px;
+}
+
+.add-category-icon {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 24px;
+  height: 24px;
+  background-color: #ccc;
+  margin-right: 10px;
+}
+
+.add-category-text {
+  flex: 1;
+}
+
+.app-container {
+  /* 原有的样式 */
+}
+
+
 </style>
